1bdabeb9212d64ed1e38e1e1d30531b3957bd521
Sofia <sofia.cronstrom@gmail.com>
2024-07-11 11:11:27 +0200

    Uppg. 1-9

--- sinus-minishop/js/script.js
+++ sinus-minishop/js/script.js
@@ -1,1 +1,44 @@
 // Code here!
+//Nr. 1
+let hoodieH3Ref = document.querySelector('.hoodie-h3');
+hoodieH3Ref.textContent = "Potato";
+
+//Nr. 2
+let NavARef = document.querySelector('header>nav>a:nth-of-type(1)');
+NavARef.textContent = "Start";
+
+//Nr. 3
+let footerARef = document.querySelector('footer>section>article:nth-of-type(1)>a:nth-of-type(3)');
+footerARef.textContent = "Mail Us";
+
+//Nr. 4
+let hoodiePRef = document.querySelector('.art-2>p');
+hoodiePRef.textContent = "This is a hoodie, red as the dangerous flame of fire. You'll look hot and you'll feel hot!";
+
+//Nr. 5
+let btnFireRef = document.querySelector('.art-2>button');
+btnFireRef.style.backgroundColor = "#FF3E0E" 
+
+//Nr. 6
+let productRef = document.querySelector('.art-3');
+productRef.style.backgroundColor = "aqua";
+
+//Nr. 7
+let adressRef = document.querySelector('footer>section>article:nth-of-type(2)>p');
+adressRef.innerHTML = "Sinus skateboards <br> Halfpipe ave. 123 <br> 1783 58, Venice beach";
+
+//Nr. 8
+let pRef = document.querySelectorAll('p');
+
+pRef.forEach( p => {
+    p.style.color = "green";
+});
+
+//Nr. 9
+let btnRef = document.querySelectorAll('button');
+
+btnRef.forEach( btn => {
+    btn.textContent = "add to cart"
+});
+
+//Nr. 10
